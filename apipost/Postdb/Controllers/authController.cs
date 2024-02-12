fdf423b3b5ed03549518db598449594de0836ff7
Quicksly24 <155920974+Quicksly24@users.noreply.github.com>
2024-02-12 15:23:33 +0100

    Update authController.cs

--- apipost/Postdb/Controllers/authController.cs
+++ apipost/Postdb/Controllers/authController.cs
@@ -46,6 +46,7 @@ public class Authcontoller:ControllerBase{
 
     public ActionResult unfollowuser(string id,string followerid){
 
+        follow.unfollow(id,followerid);    
         return Ok();
         
     }
